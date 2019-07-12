115fdecf7d8fd0f59439066e23b5f236807b0306
nima001 <1994974597@qq.com>
2019-07-12 14:07:18 +0800

    ‘添加动画‘’

--- js/main.js
+++ js/main.js
@@ -29,91 +29,97 @@ Vue.filter('currency', function (val) {
 })
 
 var vm = new Vue({
-    el: '#content',
-    data: {
-        gride1: false,
-        sd:1,
-        services: [
-            {
-                name: 'Development',
-                price: 300.00,
-                active: false,
-            },
-            {
-                name: 'Design',
-                price: 400.00,
-                active: false,
-            },
-            {
-                name: 'Integration',
-                price: 250.00,
-                active: false,
-            },
-            {
-                name: 'Training',
-                price: 220.00,
-                active: false,
-            }
-        ],
-        services1: [
-            {
-                name: 'Development',
-                price: 100.00,
-                active: false,
-            },
-            {
-                name: 'Design',
-                price: 200.00,
-                active: false,
-            },
-            {
-                name: 'Integration',
-                price: 300.00,
-                active: false,
-            },
-            {
-                name: 'Training',
-                price: 400.00,
-                active: false,
-            }
-        ]
-    },
-    methods: {
-        toggle: function (s) {
-            s.active = !s.active;
-            console.log('s', s.active);
+        el: '#content',
+        data: {
+            gride1: false,
+            sd: 1,
+            show: true,
+            services: [
+                {
+                    name: 'Development',
+                    price: 300.00,
+                    active: false,
+                },
+                {
+                    name: 'Design',
+                    price: 400.00,
+                    active: false,
+                },
+                {
+                    name: 'Integration',
+                    price: 250.00,
+                    active: false,
+                },
+                {
+                    name: 'Training',
+                    price: 220.00,
+                    active: false,
+                }
+            ],
+            services1: [
+                {
+                    name: 'Development',
+                    price: 100.00,
+                    active: false,
+                },
+                {
+                    name: 'Design',
+                    price: 200.00,
+                    active: false,
+                },
+                {
+                    name: 'Integration',
+                    price: 300.00,
+                    active: false,
+                },
+                {
+                    name: 'Training',
+                    price: 400.00,
+                    active: false,
+                }
+            ]
         },
-        total: function () {
-            var total = 0;
-            if (this.gride1) {
-                this.services.forEach(function (value) {
-                    if (value.active) {
-                        total += value.price;
-                    }
-                });
-            } else {
-                this.services1.forEach(function (value) {
-                    if (value.active) {
-                        total += value.price;
-                    }
-                });
+        methods: {
+            toggle: function (s) {
+                s.active = !s.active;
+                console.log('s', s.active);
+            },
+            chan: function () {
+                this.show = !this.show;
+            },
+            total: function () {
+                var total = 0;
+                if (this.gride1) {
+                    this.services.forEach(function (value) {
+                        if (value.active) {
+                            total += value.price;
+                        }
+                    });
+                } else {
+                    this.services1.forEach(function (value) {
+                        if (value.active) {
+                            total += value.price;
+                        }
+                    });
+                }
+                return total;
             }
-            return total;
+        },
+        watch: {
+            sd: function (data) {
+                this.sd = this.sd++;
+            }
         }
-    },
-    watch:{
-        sd:function (data) {
-            this.sd = this.sd++;
+        ,
+        mounted: function () {
+            var me = this;
+            Event.$on('change-gride', function (data) {
+                me.gride1 = data;
+                console.log(me.gride1);
+            })
         }
-    },
-    mounted: function () {
-        var me = this;
-        Event.$on('change-gride', function (data) {
-            me.gride1 = data;
-            console.log(me.gride1);
-        })
-    }
-});
-vm.$watch('sd',function (ndata,odata) {
-    document.getElementById('da').innerHTML="修改前为: " + odata + "，修改后为: " + ndata;
+    })
+;
+vm.$watch('sd', function (ndata, odata) {
+    document.getElementById('da').innerHTML = "修改前为: " + odata + "，修改后为: " + ndata;
 });
